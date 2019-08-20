d7700ad24781041802ef5a1045a6edaffbbe3207
Jesse Pires <jessepires2010@gmail.com>
2019-08-20 10:56:02 -0300

    Exercício Dois

--- A01/eDois.c
+++ A01/eDois.c
@@ -1,1 +1,34 @@
 #include <stdio.h>
+
+void inversao(int numero, int *milhares, int *centenas, int *dezenas, int *unidades);
+
+void inversao(int numero, int *milhares, int *centenas, int *dezenas, int *unidades){
+
+  *milhares = numero / 1000;
+  numero = numero % 1000;
+
+  *centenas = numero/100;
+  numero = numero % 100;
+
+  *dezenas = numero / 10;
+  numero = numero % 10;
+
+  *unidades = numero % 10;
+
+}
+
+int main(){
+
+  int numero = 1234;
+  int milhares = 0;
+  int centenas = 0;
+  int dezenas = 0;
+  int unidades = 0;
+
+  inversao(numero, &milhares, &centenas, &dezenas, &unidades);
+
+  printf("%d invertido é %d%d%d%d\n", numero, unidades, dezenas, centenas, milhares);
+
+  return 0;
+
+}
