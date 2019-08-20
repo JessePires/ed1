44d1e2639ca481e10957e2e30879e63c8e4482ea
Jesse Pires <jessepires2010@gmail.com>
2019-08-19 22:09:58 -0300

    exercício Três

---
+++ A01/eTres.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+
+char retornaConceito(float nota){
+
+  if(nota >= 9){
+
+    return 'A';
+
+  }else if(nota >= 7){
+
+    return 'B';
+
+  }else if(nota >= 6){
+
+    return 'C';
+
+  }else{
+
+    return 'I';
+
+  }
+
+}
+
+int main(){
+
+  float nota = 11;
+  char conceito;
+
+  conceito = retornaConceito(nota);
+
+  printf("Seu conceito é %c\n", conceito);
+
+  return 0;
+
+}
