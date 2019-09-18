5de7bce217c5993dc99060ed3f57f5b98511b8b8
Jesse Pires <jessepires2010@gmail.com>
2019-09-18 16:20:38 -0300

    Finalização da TAD-vetor

--- 04-TAD-vetor/TAD-vetor/TAD-vetor.h
+++ 04-TAD-vetor/TAD-vetor/TAD-vetor.h
@@ -16,34 +16,40 @@ typedef int DataType;
 typedef enum {false, true} Boolean;
 
 #define LENGTH 5
+#define OUT_OF_RANGE INT_MIN
 
 
 //PROTÓTIPOS DAS FUNÇÕES
 
 Vetor* vetor_new();
-void vetor_free(Vetor *v);
-void vetor_print(Vetor *v);
-Boolean vetor_insert(Vetor *v, DataType element, int index);
-Boolean vetor_add(Vetor *v, DataType element);
-DataType vetor_remove1(Vetor *v, int index);
-Boolean vetor_remove2(Vetor *v, int index, DataType *ptr);
-DataType vetor_shift1(Vetor *v);
-Boolean vetor_shift2(Vetor *v, DataType *ptr);
-DataType vetor_get1(Vetor *v, int index);
-DataType vetor_get2(Vetor *v, int index, DataType *ptr);
-DataType* vetor_get3(Vetor *v, int index);
-Boolean vetor_set(Vetor *v, int index, DataType valor);
-void vetor_map(Vetor *v, void (*funcao)(DataType*));
-Vetor* vetor_sub1(Vetor *v, int index);
-Vetor* vetor_sub2(Vetor *v, int start, int end);
-int vetor_size(Vetor *v);
-
+void vetor_free(Vetor* v);
+void vetor_print(Vetor* v);
+Boolean vetor_insert(Vetor* v, DataType element, int index);
+Boolean vetor_add(Vetor* v, DataType element);
+DataType vetor_remove1(Vetor* v, int index);
+Boolean vetor_remove2(Vetor* v, int index, DataType *ptr);
+DataType vetor_shift1(Vetor* v);
+Boolean vetor_shift2(Vetor* v, DataType *ptr);
+void vetor_shiftAdd(Vetor* v, int position);
+void vetor_shiftRemove(Vetor* v, int position);
+DataType vetor_get1(Vetor* v, int index);
+Boolean vetor_get2(Vetor* v, int index, DataType *ptr);
+DataType* vetor_get3(Vetor* v, int index);
+Boolean vetor_set(Vetor* v, int index, DataType valor);
+void vetor_map(Vetor* v, void (*funcao)(DataType*));
+Vetor* vetor_sub1(Vetor* v, int index);
+Vetor* vetor_sub2(Vetor* v, int start, int end);
+int vetor_size(Vetor* v);
+Boolean vetor_isFull(Vetor* v);
+Boolean vetor_haveSpace(Vetor* v);
+void vetor_increase(Vetor* v);
+void vetor_decrease(Vetor* v);
 
 //OPERAÇÕES
 
 Vetor* vetor_new(){
 
-    Vetor *vet = (Vetor*) malloc(sizeof(Vetor));
+    Vetor* vet = (Vetor*) malloc(sizeof(Vetor));
     vet->vetor = (DataType*) calloc(5, sizeof(DataType));
     vet->size = 0;
     vet->length = 5;
@@ -52,44 +58,228 @@ Vetor* vetor_new(){
 
 }
 
-void vetor_free(Vetor *v){
+void vetor_free(Vetor* v){
 
     free(v->vetor);
     free(v);
 
 }
 
-void vetor_print(Vetor *v){
+void vetor_print(Vetor* v){
 
-    printf("[");
+    printf("[\t");
 
     for(int i = 0; i < v->size; i++){
 
         printf("%d\t", v->vetor[i]);
 
-        if(i == v->size -1){
+    }
 
-            printf("%d]\n", v->vetor[i]);
+    printf("]\n");
+
+}
+
+Boolean vetor_insert(Vetor* v, DataType element, int index){
+
+    if(index < v->size){
+
+        vetor_shiftAdd(v, index);
+        v->vetor[index] = element;
+
+        return true;
+
+    }
+
+    return false;
+
+}
+
+Boolean vetor_add(Vetor* v, DataType element){
+
+    if(v != NULL){
+
+        if(vetor_isFull(v)){
+
+            vetor_increase(v);
+
+        }
+
+        if(element != OUT_OF_RANGE){
+
+            v->vetor[v->size] = element;
+            v->size++;
+            return true;
+
+        }
+
+        return false;
+
+    }
+
+    return false;
+
+}
+
+DataType vetor_remove1(Vetor* v, int index){
+
+    DataType removedElement = OUT_OF_RANGE;
+
+    if(index < v->size){
+
+        removedElement = v->vetor[index];
+
+        if(index > 0){
+
+            vetor_shiftRemove(v, index);
+        
+            v->size--;
+
+        }else{
+
+            vetor_shift1(v);
+
+        }
+
+        if(vetor_haveSpace(v)){
+
+            vetor_decrease(v);
 
         }
 
     }
 
+    return removedElement;
+
 }
 
-Boolean vetor_insert(Vetor *v, DataType element, int index){}
+Boolean vetor_remove2(Vetor* v, int index, DataType *ptr){
 
-Boolean vetor_add(Vetor *v, DataType element){}
+    *ptr = OUT_OF_RANGE;
 
-DataType vetor_remove1(Vetor *v, int index);
+    if(index < v->size){
 
-Boolean vetor_remove2(Vetor *v, int index, DataType *ptr){}
+        *ptr = v->vetor[index];
 
-DataType vetor_shift1(Vetor *v){}
+        if(index > 0){
 
-Boolean vetor_shift2(Vetor *v, DataType *ptr){}
+            vetor_shiftRemove(v, index);
+        
+            v->size--;
 
-DataType vetor_get1(Vetor *v, int index){
+        }else{
+
+            vetor_shift1(v);
+
+        }
+
+        if(vetor_haveSpace(v)){
+
+            vetor_decrease(v);
+
+        }
+
+        return true;
+
+    }
+
+    return false;
+
+}
+
+DataType vetor_shift1(Vetor* v){
+
+    DataType aux = v->vetor[0];
+
+    for(int i = 1; i < v->size; i++){
+
+        v->vetor[i-1] = v->vetor[i];
+
+    }
+
+    v->size--;
+
+    if(vetor_haveSpace(v)){
+
+        vetor_decrease(v);
+
+    }
+
+    return aux;
+
+}
+
+Boolean vetor_shift2(Vetor* v, int* ptr){
+
+    if(v != NULL){
+
+        *ptr = v->vetor[0];
+
+        for(int i = 1; i < v->size; i++){
+
+            v->vetor[i-1] = v->vetor[i];
+
+        }
+
+        v->size--;
+
+        if(vetor_haveSpace(v)){
+
+            vetor_decrease(v);
+
+        }
+
+        return true;
+
+    }
+
+    return false;
+
+}
+
+void vetor_shiftAdd(Vetor* v, int position){
+
+    DataType aux1, aux2;
+
+    if(vetor_isFull(v)){
+
+        vetor_increase(v);
+
+    }
+
+    aux1 = v->vetor[position];
+    aux2 = v->vetor[position+1];
+
+    for(int i = position + 1; i <= v->size + 1; i++){
+
+        v->vetor[i] = aux1;
+        aux1 = aux2;
+        aux2 = v->vetor[i+1];
+
+    }
+
+    v->size++;
+
+}
+
+void vetor_shiftRemove(Vetor* v, int position){
+
+    if(position > 0){
+
+        for(int i = position -1; i < v->size; i++){
+
+            v->vetor[i] = v->vetor[i+1];
+
+        }   
+
+    }else{
+
+        vetor_shift1(v);
+
+    }
+
+}
+
+DataType vetor_get1(Vetor* v, int index){
 
     if(index < v->size){
 
@@ -97,16 +287,55 @@ DataType vetor_get1(Vetor *v, int index){
 
     }
 
-    return INT_MIN;
+    return OUT_OF_RANGE;
 
 }
 
-Boolean vetor_get2(Vetor *v, int index, DataType *ptr){
+Boolean vetor_get2(Vetor* v, int index, DataType* ptr){
 
     if(index < v->size){
 
-        ptr = &v->vetor[size];
+        ptr = &v->vetor[index];
         return true;
+
+    }
+
+    return false;
+    
+}
+
+DataType* vetor_get3(Vetor* v, int index){
+
+    DataType* element = (DataType*) malloc(sizeof(DataType));
+    DataType aux;
+
+    if(index < v->size){
+
+        aux = v->vetor[index];
+
+    }else{
+
+        aux = OUT_OF_RANGE;
+
+    }
+
+    element = &aux;
+    return element;
+
+}
+
+Boolean vetor_set(Vetor* v, int index, DataType valor){
+
+    if(v->size > 0){
+
+        if(index < v->size){
+
+            v->vetor[index] = valor;
+            return true;
+
+        }
+
+        return false;
 
     }
 
@@ -114,29 +343,110 @@ Boolean vetor_get2(Vetor *v, int index, DataType *ptr){
 
 }
 
-DataType* vetor_get3(Vetor *v, int index){}
+void vetor_map(Vetor* v, void (*funcao)(DataType*)){
 
-Boolean vetor_set(Vetor *v, int index, DataType valor){
+    for(int i = 0; i < v->size; i++){
 
-    if(index < v->size){
-
-        v->vetor[index] = valor;
-        return true;
+       funcao(&v->vetor[i]);
 
     }
 
-    return false;
+}
+
+Vetor* vetor_sub1(Vetor* v, int index){
+
+    Vetor* newVector = vetor_new(); 
+
+    index = (index < 0 ? v->size + index : index);
+
+    for(int i = index; i <= v->size -1 ; i++){
+
+        vetor_add(newVector, v->vetor[i]);
+
+    }
+
+    return newVector;
 
 }
 
-void vetor_map(Vetor *v, void (*funcao)(DataType*)){}
+Vetor* vetor_sub2(Vetor* v, int start, int end){
 
-Vetor* vetor_sub1(Vetor *v, int index){}
+    Vetor* newVector = vetor_new(); 
 
-Vetor* vetor_sub2(Vetor *v, int start, int end){}
+    if(start < end){
 
-int vetor_size(Vetor *v){
+        start = (start < 0 ? v->size + start : start);
+
+        for(int i = start; i < end; i++){
+
+            vetor_add(newVector, v->vetor[i]);
+
+        }
+
+    }
+
+    return newVector;
+
+}
+
+int vetor_size(Vetor* v){
 
     return v->size;
+
+}
+
+Boolean vetor_isFull(Vetor* v){
+
+    if(v->size == v->length){
+
+        return true;
+
+    }
+
+    return false;
+
+}
+
+Boolean vetor_haveSpace(Vetor* v){
+
+    if(v->size < v->length * 0.25){
+
+        return true;
+
+    }
+
+    return false;
+
+}
+
+void vetor_increase(Vetor* v){
+
+    DataType* vector = (DataType*) calloc(v->length * 2, sizeof(DataType));
+
+    for(int i = 0; i < v->size; i++){
+
+        vector[i] = v->vetor[i];
+
+    }
+
+    free(v->vetor);
+    v->length *= 2;
+    v->vetor = vector;
+
+}
+
+void vetor_decrease(Vetor* v){
+
+    DataType* vector = (DataType*) calloc(v->length / 2, sizeof(DataType));
+
+    for(int i = 0; i < v->size; i++){
+
+        vector[i] = v->vetor[i];
+
+    }
+
+    free(v->vetor);
+    v->length -= 5;
+    v->vetor = vector;
 
 }
--- 04-TAD-vetor/TAD-vetor/main.c
+++ 04-TAD-vetor/TAD-vetor/main.c
@@ -1,8 +1,99 @@
 #include <stdio.h>
-#include <TAD-vetor.h>
+#include "TAD-vetor.h"
+
+void dobraValores(int* elem){
+
+    *elem *= 2;
+
+}
 
 int main(){
 
+    //STATEMENTS
+
+    Vetor* v1 = vetor_new();
+    Vetor* subV1;
+    Vetor* sub2V1;
+    int excludedElement;
+    int excludedElementPtr;
+    int removedElement;
+    int removedElementptr;
+    int elem1;
+
+    //MANIPULATIONS
+
+    //adições
+    vetor_add(v1, 1);
+    vetor_add(v1, 2);
+    vetor_add(v1, 3);
+    vetor_add(v1, 4);
+    vetor_add(v1, 5);
+    vetor_add(v1, 6);
+    vetor_add(v1, 7);
+
+    printf("Vetor 1: ");
+    vetor_print(v1);
+    printf("Size: %d\n", vetor_size(v1));
+
+    //inserção
+    if(vetor_insert(v1, 10, 2)){
+
+        printf("Vetor 1 com valor inserido: ");
+        vetor_print(v1);
+        printf("Size: %d\n", vetor_size(v1));
+
+    }else{
+
+        printf("Impossível realizar inserção!\n");
+
+    }
+
+
+    //shift
+    excludedElement = vetor_shift1(v1);
+    printf("Elemento excluído: %d\n", excludedElement);
+    excludedElement = vetor_shift1(v1);
+    printf("Elemento excluído: %d\n", excludedElement);
+    excludedElement = vetor_shift1(v1);
+    printf("Elemento excluído: %d\n", excludedElement);
+
+    printf("Vetor V1 após o shift: ");
+    vetor_print(v1);
+    printf("Size: %d\n", vetor_size(v1));
+
+    vetor_shift2(v1, &excludedElementPtr);
+    printf("Elemento excluído: %d\n", excludedElementPtr);
+    vetor_shift2(v1, &excludedElementPtr);
+    printf("Elemento excluído: %d\n", excludedElementPtr);
+
+    printf("Vetor após o shift: ");
+    vetor_print(v1);
+
+    //remoção
+    removedElement = vetor_remove1(v1, 0);
+    printf("Elemento removido: %d\n", removedElement);
+    vetor_print(v1);
+
+    //criação de subvetores
+    subV1 = vetor_sub1(v1, -1);
+    printf("Sub-vetor de V1: ");
+    vetor_print(subV1);
+
+    sub2V1 = vetor_sub2(v1, 0, 1);
+    printf("Sub-vetor 2 de V1: ");
+    vetor_print(sub2V1);
+
+    //get
+    elem1 = vetor_get1(subV1, 0);
+    printf("elemeno obtido com vetor_get1(): %d\n", elem1);
+
+    printf("elemento obtico com vetor_get2(): %d\n", vetor_get2(v1, 0, &elem1));
+
+    //teste função map
+    vetor_map(subV1, dobraValores);
+    printf("Sub-vetor1 após a aplicação da função dobraValores(): ");
+    vetor_print(subV1);
+    
     return 0;
 
 }
