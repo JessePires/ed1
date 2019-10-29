febc3b6fd273e3d8fb75030a9b0e0ebb73847399
Jesse Pires <jessepires2010@gmail.com>
2019-10-29 12:36:08 -0300

    Implementação da fila com a estratégia de encadeamento

--- 08-fila/FilaEncadeada.h
+++ 08-fila/FilaEncadeada.h
@@ -1,7 +1,8 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <limits.h>
 
-
+#define NOT_FOUND_ELEMENT INT_MAX;
 
 typedef enum boolean{false=0, true=1} Boolean;
 typedef int Tipo;
@@ -27,32 +28,156 @@ typedef struct{
 //PROTÓTIPOS
 
 Fila* fila_criar();
-void fila_destruir(Fila* f);
+void no_destruir(No* no);
+Fila* fila_destruir(Fila* f);
 Boolean fila_inserir(Fila* f, Tipo elemento);
 Tipo* fila_remover1(Fila* f);
 Boolean fila_remover2(Fila* f, Tipo* endereco);
 Boolean fila_primeiro(Fila* f, Tipo* endereco);
 int fila_tamanho(Fila* f);
 int fila_contem(Fila* f, Tipo elemento);
+void fila_imprimir(Fila* f);
 
 
 //IMPLEMENTAÇÕES
 
 Fila* fila_criar(){
 
-    return (Fila*) malloc(sizeof(Fila));
+    Fila* f = (Fila*) malloc(sizeof(Fila));
+    f->qtde = 0;
+
+    return f;
  
 }
 
-void fila_destruir(Fila* f){
+void no_destruir(No* no){
 
+    if(no == NULL) return;
 
+    no_destruir(no->prox);
+    free(no);
 
 }
 
-Boolean fila_inserir(Fila* f, Tipo elemento);
-Tipo* fila_remover1(Fila* f);
-Boolean fila_remover2(Fila* f, Tipo* endereco);
-Boolean fila_primeiro(Fila* f, Tipo* endereco);
-int fila_tamanho(Fila* f);
-int fila_contem(Fila* f, Tipo elemento);
+Fila* fila_destruir(Fila* f){
+
+    no_destruir(f->prim);
+    free(f->ult);
+    return NULL;
+
+}
+
+Boolean fila_inserir(Fila* f, Tipo elemento){
+
+    if(f != NULL){
+        
+        No* no = (No*) malloc(sizeof(No));
+        no->dado = elemento;
+
+        if(f->prim == NULL){
+            
+            f->prim = no;
+            f->ult = no;
+            f->qtde++;
+
+        }else{
+
+            No* aux = f->ult;
+            f->ult = no;
+            aux->prox = f->ult;        
+            f->qtde++;
+
+        }
+
+        return true;
+        
+    }
+
+    return false;
+
+}
+
+Tipo* fila_remover1(Fila* f){
+
+    Tipo* endereco = (Tipo*) malloc(sizeof(Tipo));
+    endereco = &f->prim->dado;
+    f->prim = f->prim->prox;  
+    f->qtde--;  
+    return endereco;
+
+}
+
+Boolean fila_remover2(Fila* f, Tipo* endereco){
+
+    if(f != NULL){
+
+        *endereco = f->prim->dado;
+        f->prim = f->prim->prox;
+        f->qtde--;
+        return true;
+    }
+    
+    return false;
+
+}
+
+Boolean fila_primeiro(Fila* f, Tipo* endereco){
+
+    if(f != NULL){
+
+        *endereco = f->prim->dado;
+        return true;
+
+    }
+
+    return false;
+
+}
+
+int fila_tamanho(Fila* f){
+
+    return f->qtde;
+
+}
+
+int fila_contem(Fila* f, Tipo elemento){
+
+    No* aux = f->prim;
+
+    while(aux != NULL){
+
+        if(aux->dado == elemento){
+
+            return aux->dado;
+
+        }
+
+        aux = aux->prox;
+
+    }
+
+    return NOT_FOUND_ELEMENT;
+
+}
+
+void fila_imprimir(Fila* f){
+
+    if(f != NULL){
+
+        No* aux = f->prim;
+        printf("Fila encadeada: [");
+        while(aux != NULL){
+
+            printf("%d", aux->dado);
+            printf("%s", (aux->prox == NULL ? "]\n" : ", "));
+            aux = aux->prox;    
+
+        }
+
+    }else{
+
+        printf("Impossível imprimir a fila!\n");
+
+    }
+
+}
--- 08-fila/testeEncadeada.c
+++ 08-fila/testeEncadeada.c
@@ -1,9 +1,53 @@
 #include <stdio.h>
 #include "FilaEncadeada.h"
 
+
 void teste(){
 
-    
+    //TESTE DE CRIAÇÃO DA FILA
+    Fila* f = fila_criar();
+
+    //TESTE DE INSERÇÃO
+    fila_inserir(f, 10);
+    fila_inserir(f, 20);
+    fila_inserir(f, 30);    
+    fila_inserir(f, 40);    
+    fila_inserir(f, 50);
+
+    fila_imprimir(f);
+
+    //TESTE DE REMOÇÃO
+    int* removido = fila_remover1(f);
+    printf("Elemento removido: %d\n", *removido);
+    int removido2;
+    fila_remover2(f, &removido2);
+    printf("Segundo elemento removido: %d\n", removido2);
+
+    fila_imprimir(f);
+
+    //TESTE DE VERIFICAÇÃO DO PRIMEIRO ELEMENTO
+    int primeiro;
+    fila_primeiro(f, &primeiro);
+    printf("O primeiro elemento da fila é %d\n", primeiro);
+
+    //TESTE DA FUNÇÃO fila_tamanho()
+    printf("A fila possui o tamanho = %d\n", fila_tamanho(f));
+
+    //TESTE DA FUNÇÃO fila_contem()
+    int existe = fila_contem(f, 30);
+    if(existe == 30){
+
+        printf("A fila contem o elemento!\n");
+
+    }else{
+
+        printf("A fila não contem o elemento!\n");
+
+    }
+
+    //TESTE DE DESTRUIÇÃO
+    f = fila_destruir(f);
+    fila_imprimir(f);
 
 }
 
