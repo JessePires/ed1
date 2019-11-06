122c4e0e63db768254ca759578df56dd8d4b7786
Jesse Pires <jessepires2010@gmail.com>
2019-11-06 19:26:23 -0300

    Implementação de fila com prioridade nas estratégias encadeada e circular (esta última apresenta erro)

---
+++ fila-com-prioridade/FilaCircular.h
@@ -0,0 +1,218 @@
+
+#include <stdio.h>
+#include <stdlib.h>
+
+
+//TIPOS DE DADOS
+typedef enum boolean{false=0, true=1} Boolean;
+typedef int Tipo;
+
+typedef struct{
+    Tipo* vetor;
+    int tam;
+    int inicio;
+    int fim;
+}Fila;
+
+//PROTÓTIPOS
+
+Fila* fila_criar();
+Fila* fila_destruir(Fila* f);
+int retornaPosicao(Fila* f, Tipo Elemento);
+void shift(Fila* f, int posicao);
+Boolean fila_inserir(Fila* f, Tipo elemento); //TENTAR IMPLEMENTAR OLHANDO SÓ PARA O INÍCIO E O FIM
+                                              //USAR MOD DE 5 PARA ATUALIZAR O FIM  
+Tipo* fila_remover1(Fila* f);
+Boolean fila_remover2(Fila* f, Tipo* endereco);
+Boolean fila_primeiro(Fila* f, Tipo* endereco);
+int fila_tamanho(Fila* f);
+Boolean fila_contem(Fila* f, Tipo elemento);
+void fila_imprimir(Fila* f);
+
+
+//IMPLEMENTAÇÕES
+
+Fila* fila_criar(){
+
+    Fila* f = (Fila*) malloc(sizeof(Fila));
+    f->tam = 5;
+    f->vetor = (Tipo*) calloc(5, sizeof(Tipo));
+    f->inicio = 0;
+    f->fim = 0;
+
+    return f;
+
+}
+
+Fila* fila_destruir(Fila* f){
+
+    free(f->vetor);
+    free(f);
+    return NULL;
+
+}
+
+//auxiliar de fila_inserir()
+void fila_garanteEspaco(Fila* f){
+
+    if(f->inicio != (f->fim + 1) % f->tam) return;
+    
+    Tipo* v = (Tipo*) calloc(f->tam * 2, sizeof(Tipo));
+    for(int i = f->inicio; i < f->fim; i++){
+
+        v[i % f->tam] = f->vetor[i % f->tam];
+
+    }
+
+    f->vetor = v;
+    f->tam += 5;
+
+}
+
+int retornaPosicao(Fila* f, Tipo elemento){
+
+    int cont = 0;
+    int i = 0;
+
+    while(i < (f->fim % f->tam) && f->vetor[i] > elemento){
+
+        cont++;
+
+    }
+
+    return cont;
+
+}
+
+void shift(Fila* f, int posicao){
+
+    for(int i = f->fim; i > posicao; i--){
+
+        f->vetor[(i % f->tam) + 1] = f->vetor[i % f->tam];
+
+    }
+
+}
+
+Boolean fila_inserir(Fila* f, Tipo elemento){
+
+    fila_garanteEspaco(f);
+
+    if(f != NULL){
+
+        if(f->vetor[f->inicio] < elemento){
+
+            shift(f, 0);
+            f->vetor[f->inicio] = elemento;
+
+        }else if(f->vetor[f->fim] > elemento){
+
+            f->vetor[f->fim] = elemento;
+
+        }else{
+
+            int posicao = retornaPosicao(f, elemento);
+            shift(f, posicao);
+            f->vetor[posicao] = elemento;
+
+        }
+        
+        f->vetor[f->fim] = elemento;
+        f->fim++;
+        return true;
+
+    }
+
+    return false;
+
+}
+
+Tipo* fila_remover1(Fila* f){
+
+    if(f->inicio == f->fim){
+
+        return NULL;
+
+    }
+
+    Tipo* endereco = (Tipo*) malloc(sizeof(Tipo));
+    endereco = &f->vetor[f->inicio];
+    f->inicio++;
+    return endereco;
+
+}
+
+Boolean fila_remover2(Fila* f, Tipo* endereco){
+
+    if(f->inicio == 0 && f->fim == 0){
+
+        return false;
+
+    }
+
+    *endereco = f->vetor[f->inicio];
+    f->inicio++;
+    return true;
+
+}
+
+Boolean fila_primeiro(Fila* f, Tipo* endereco){
+
+    if(f != NULL){
+
+        *endereco = f->vetor[f->inicio];
+        return true;
+
+    }
+
+    return false;
+
+}
+
+int fila_tamanho(Fila* f){
+
+    if(f != NULL){
+
+        return f->tam;
+
+    }
+
+    return -1;
+
+}
+
+Boolean fila_contem(Fila* f, Tipo elemento){
+
+    for(int i = f->inicio; i < f->fim; i++){
+
+        if(f->vetor[i % f->tam] == elemento){
+
+            return true;
+
+        }
+
+    }
+
+    return false;
+
+}
+
+void fila_imprimir(Fila* f){
+
+    if(f != NULL){
+
+        printf("Fila circular <f>: [");
+        for(int i = f->inicio; i < f->fim; i++){
+
+            printf("%d", f->vetor[i % f->tam]);
+            printf("%s", (i < f->fim % f->tam - 1 ? ", " : "]\n"));
+
+        }
+
+    }else{
+
+        printf("Impossível imprimir a fila!\n");
+
+    }
+
+}
---
+++ fila-com-prioridade/testeCircular.c
@@ -0,0 +1,65 @@
+#include <stdio.h>
+#include "FilaCircular.h"
+
+void teste(){
+
+    //TESTE DE CRIAÇÃO DA FILA
+    Fila* f = fila_criar();
+
+    //TESTE DE INSERÇÃO
+    fila_inserir(f, 10);
+    fila_inserir(f, 20);
+    fila_inserir(f, 30);    
+    fila_inserir(f, 40);    
+    fila_inserir(f, 50);
+    fila_inserir(f, 25);
+
+    fila_imprimir(f);
+
+    //TESTE DE REMOÇÃO
+    int* removido = fila_remover1(f);
+    printf("Elemento removido: %d\n", *removido);
+    int removido2;
+    fila_remover2(f, &removido2);
+    printf("Segundo elemento removido: %d\n", removido2);
+
+    fila_imprimir(f);
+
+    //SEGUNDO TESTE DE INSERÇÃO
+    fila_inserir(f, 60);
+    fila_inserir(f, 70); 
+    fila_inserir(f, 80);    
+
+    fila_imprimir(f);
+
+    int primeiro;
+    fila_primeiro(f, &primeiro);
+    printf("O primeiro elemento da fila é %d\n", primeiro);
+
+    //TESTE DA FUNÇÃO fila_tamanho()
+    printf("A fila possui o tamanho = %d\n", fila_tamanho(f));
+
+    //TESTE DA FUNÇÃO fila_contem()
+    int valorTeste = 30;
+    if(fila_contem(f, valorTeste)){
+
+        printf("A fila contem o elemento %d!\n", valorTeste);
+
+    }else{
+
+        printf("A fila não contem o elemento %d!\n", valorTeste);
+
+    }
+
+    //TESTE DE DESTRUIÇÃO
+    f = fila_destruir(f);
+    fila_imprimir(f);
+
+}
+
+int main(){
+
+    teste();
+    return 0;
+
+}
